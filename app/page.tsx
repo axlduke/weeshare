1d7a09e44d670d3561931cac5030922d9e2e2be6
ace malto <maltoace9@gmail.com>
2024-06-26 23:13:14 +0800

    modify with multiple fileType

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,113 @@
+"use client"
 import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
+import { FaFacebookSquare, FaInstagram, FaGithubSquare, FaLinkedin } from "react-icons/fa";
 
 export default function Home() {
+
+  const [file, setFile] = useState<File | null>(null);
+  const [fileUrl, setFileUrl] = useState<string>('');
+  const [error, setError] = useState<string>('');
+
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const selectedFile = e.target.files?.[0] || null;
+    setFile(selectedFile);
+  };
+
+  const handleUpload = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setError(''); // Reset error state
+
+    if (!file) {
+      setError('No file selected');
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append('file', file);
+
+    try {
+      const res = await fetch('/api/upload', {
+        method: 'POST',
+        body: formData,
+      });
+
+      if (!res.ok) {
+        const errorData = await res.json();
+        setError(`Error uploading file: ${errorData.message}`);
+        return;
+      }
+
+      const data = await res.json();
+      setFileUrl(data.fileUrl);
+    } catch (error) {
+      const errorMessage = error instanceof Error ? error.message : 'An unknown error occurred';
+      setError(`Request failed: ${errorMessage}`);
+    }
+  };
+
   return (
     <div className="flex justify-center items-center h-screen bg-slate-300">
-      <div className="border w-[440px] h-[490px] bg-white rounded-lg shadow-xl">
+      <div className="border w-[440px] h-[440px] bg-white rounded-lg shadow-xl">
         <div className="space-y-3 pt-5 mx-5">
           <div>
             <h1 className="font-sans font-bold">Weee Shareee</h1>
             <span className="text-sm text-slate-400 font-sans">Hellooo!!!!!</span>
           </div>
           
-          <div className="flex items-center justify-center w-full">
-              <label htmlFor="dropzone-file" className="flex flex-col group items-center justify-center w-full h-56 border-2 border-gray-300 border-dashed hover:border-orange-500 hover:transition hover:delay-150 hover:ease-in-out hover:duration-300 rounded-lg cursor-pointer bg-gray-50 ">
-                  <div className="flex flex-col items-center justify-center pt-5 pb-6">
-                      <svg className="w-8 h-8 mb-4 text-gray-500 group-hover:text-orange-500 hover:transition hover:delay-150 hover:ease-in-out hover:duration-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
-                          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"/>
-                      </svg>
-                      <p className="mb-2 text-sm text-gray-500 group-hover:text-orange-500 hover:transition hover:delay-150 hover:ease-in-out hover:duration-300"><span className="font-semibold">Click to upload</span> or drag and drop</p>
-                      <p className="text-xs text-gray-500 group-hover:text-orange-500 hover:transition hover:delay-150 hover:ease-in-out hover:duration-300">SVG, PNG, JPG, GIF, PDF, DOCS, MP3, ETC. </p>
+
+          {/* UPLOADING SECTION */}
+          <form onSubmit={handleUpload} className="space-y-3">
+
+            <div className="flex items-center justify-center w-full">
+                <label htmlFor="dropzone-file" className="flex flex-col group items-center justify-center w-full h-56 border-2 border-gray-300 border-dashed hover:border-orange-500 hover:transition hover:delay-150 hover:ease-in-out hover:duration-300 rounded-lg cursor-pointer bg-gray-50 ">
+                    <div className="flex flex-col items-center justify-center pt-5 pb-6">
+                        <svg className="w-8 h-8 mb-4 text-gray-500 group-hover:text-orange-500 hover:transition hover:delay-150 hover:ease-in-out hover:duration-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"/>
+                        </svg>
+                        <p className="mb-2 text-sm text-gray-500 group-hover:text-orange-500 hover:transition hover:delay-150 hover:ease-in-out hover:duration-300"><span className="font-semibold">Click to upload</span> or drag and drop</p>
+                        <p className="text-xs text-gray-500 group-hover:text-orange-500 hover:transition hover:delay-150 hover:ease-in-out hover:duration-300">SVG, PNG, JPG, GIF, PDF, DOCS, MP3, ETC. </p>
+                    </div>
+                    <input id="dropzone-file" type="file" className="hidden" onChange={handleFileChange}/>
+                </label>
+            </div> 
+
+            {/* URL copying url */}
+            <div className="flex items-center space-x-1">
+              {
+                fileUrl &&
+                <input type="text" className="w-64 text-sm font-serif outline-none cursor-default py-1" readOnly placeholder="dsa" value={fileUrl}/>
+              }
+              {
+                error && (
+                  <div className="w-64 text-red-400 text-sm font-serif outline-none cursor-default py-1">
+                    <p>{error}</p>
                   </div>
-                  <input id="dropzone-file" type="file" className="hidden" />
-              </label>
-          </div> 
+                )
+              }
+              <button className="text-sm border px-8 py-1 rounded-md hover:text-orange-700 hover:bg-orange-400 hover:transition hover:delay-75 hover:ease-in-out hover:duration-300">
+                COPY URL
+              </button>
+            </div>
 
-          <div>
-
+            <div className="flex justify-between items-center">
+              <div className="">
+                <h1 className="text-xs">Help Center</h1>
+              </div>
+              <div className="flex space-x-1">
+                <button className="text-sm border px-8 py-1 rounded-md">Cancel</button>
+                <button type="submit" className="text-sm border px-8 py-1 rounded-md hover:text-orange-700 hover:bg-orange-400 hover:transition hover:delay-75 hover:ease-in-out hover:duration-300">Upload</button>
+              </div>
+            </div>
+          </form>
+          {/* FaFacebookSquare, FaInstagram, FaGithubSquare, FaLinkedin */}
+          <div className="flex items-center space-x-2">
+            <h1 className="text-xs">💖 ace_kun</h1>
+            <span className="text-lg"><FaFacebookSquare /></span>
+            <span className="text-lg"><FaInstagram/></span>
+            <span className="text-lg"><Link href='https://github.com/axlduke'><FaGithubSquare/></Link></span>
+            <span className="text-lg"><FaLinkedin/></span>
           </div>
 
         </div>
--- pages/api/upload.js
+++ pages/api/upload.js
@@ -1,13 +1,54 @@
+// import { IncomingForm } from 'formidable';
+// import fs from 'fs';
+// import path from 'path';
+
+// const uploadDir = path.resolve('./public/uploads');
+
+// if (!fs.existsSync(uploadDir)) {
+//   fs.mkdirSync(uploadDir, { recursive: true });
+// }
+
+// export const config = {
+//   api: {
+//     bodyParser: false,
+//   },
+// };
+
+// export default function handler(req, res) {
+//   const form = new IncomingForm({ uploadDir, keepExtensions: true });
+
+//   form.parse(req, (err, fields, files) => {
+//     if (err) {
+//       return res.status(500).json({ message: 'File upload failed', error: err });
+//     }
+
+//     if (!files.file) {
+//       return res.status(400).json({ message: 'No file uploaded' });
+//     }
+
+//     const file = files.file[0];
+//     const filePath = file.filepath;
+//     const fileName = file.newFilename;
+//     const fileUrl = `${process.env.NEXT_PUBLIC_BASE_URL}/uploads/${fileName}`;
+
+
+//     return res.status(200).json({ message: 'File uploaded successfully', fileUrl });
+//   });
+// }
+
 import { IncomingForm } from 'formidable';
 import fs from 'fs';
 import path from 'path';
 
+// Directory to save uploaded files
 const uploadDir = path.resolve('./public/uploads');
 
+// Ensure the upload directory exists
 if (!fs.existsSync(uploadDir)) {
   fs.mkdirSync(uploadDir, { recursive: true });
 }
 
+// Configuration to prevent Next.js from parsing the body automatically
 export const config = {
   api: {
     bodyParser: false,
@@ -15,10 +56,16 @@ export const config = {
 };
 
 export default function handler(req, res) {
-  const form = new IncomingForm({ uploadDir, keepExtensions: true });
+  const form = new IncomingForm({
+    uploadDir, // Directory to store uploaded files
+    keepExtensions: true, // Keep the file extension
+    maxFileSize: 20 * 1024 * 1024 * 1024, // Set file size limit to 20GB
+    multiples: false, // Only accept single file
+  });
 
   form.parse(req, (err, fields, files) => {
     if (err) {
+      console.error('Error parsing form:', err);
       return res.status(500).json({ message: 'File upload failed', error: err });
     }
 
@@ -26,21 +73,28 @@ export default function handler(req, res) {
       return res.status(400).json({ message: 'No file uploaded' });
     }
 
-    const file = files.file;
-    const filePath = file.path;
+    const file = files.file[0];
+    const filePath = file.filepath
+    const fileName = file.newFilename
 
-    // Debugging log
+    // Debugging logs
     console.log('Files:', files);
-    console.log('File:', file);
+    console.log('File Path:', filePath);
+    console.log('File Name:', fileName);
 
-    // Check if filePath is a valid string
+    // Handle invalid file path
     if (!filePath || typeof filePath !== 'string') {
-        return res.status(500).json({ message: 'Invalid file path received' });
+      return res.status(500).json({ message: 'Invalid file path received' });
     }
 
-    const fileName = path.basename(filePath);
+    // Ensure correct path format and escape backslashes
+    const normalizedFilePath = path.normalize(filePath);
+    console.log(`Normalized File Path: ${String.raw`${normalizedFilePath}`}`);
+
+    // Construct the file URL
     const fileUrl = `${process.env.NEXT_PUBLIC_BASE_URL}/uploads/${fileName}`;
+    console.log('Generated fileUrl:', fileUrl);
 
     return res.status(200).json({ message: 'File uploaded successfully', fileUrl });
-    });
+  });
 }
